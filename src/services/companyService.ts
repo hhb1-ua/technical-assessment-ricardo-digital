87593c4a8cc9e7a6f4c821a59b8385a2029ec0e8
hhb1-ua <hhb1@alu.ua.es>
2024-08-30 20:29:10 +0200

    Created company service

---
+++ src/services/companyService.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { z } from 'zod';
+import { Company } from '../types/Company';
+
+// CAMBIAR ESTO EN EL FUTURO
+const HOST = 'http://localhost:3000';
+
+const companySchema = z.object({
+  cif: z.string().regex(/^([ABCDEFGHJKLMNPQRSUVW])(\d{7})([0-9A-J])$/),
+  name: z.string()
+});
+
+export const createCompany = async (company: Omit<Company, 'id'>): Promise<Company> => {
+  companySchema.parse(company);
+
+  return (await axios.post<Company>(`${HOST}/company`, company)).data;
+}
+
+export const readCompanies = async (): Promise<Company[]> => {
+  return (await axios.get<Company[]>(`${HOST}/company`)).data;
+}
+
+export const updateCompany = async (company: Company): Promise<Company> => {
+  companySchema.parse(company);
+
+  return (await axios.put<Company>(`${HOST}/company/${company.id}`, company)).data;
+}
+
+export const deleteCompany = async (company: Company): Promise<Company> => {
+  return (await axios.delete<Company>(`${HOST}/company/${company.id}`)).data;
+}
